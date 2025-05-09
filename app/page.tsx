2ba4cc75f6ce99b2d4bd0944019aeb3adfc8e1c7
Dryha Andriy <driga.and@gmail.com>
2025-05-10 00:13:20 +0300

    add background

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import "@/app/page.scss";
 import Header from "./components/header/header";
 import Hero from "./components/hero/hero";
 import Contacts from "./components/contacts/contacts";
+import Background from "./components/background/background";
 
 export default function Home() {
   return (
     <div className="container">
+      <Background />
       <Header />
       <Hero />
       <Contacts />
