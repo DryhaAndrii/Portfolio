dbe5854daebf1e541f41a5b1406baf21d6a5ab99
Dryha Andriy <driga.and@gmail.com>
2025-05-14 22:38:28 +0300

    small changes

---
+++ app/components/technologies/stack.data.ts
@@ -0,0 +1,23 @@
+export const stack = [
+  {
+    title: "Frontend",
+    stack: [
+      "React, Next.js",
+      "HTML, CSS, JavaScript, TypeScript",
+      "SCSS, Tailwind, MaterialUI",
+      "Redux, Zustand, Jotai",
+    ],
+  },
+  {
+    title: "Backend",
+    stack: [
+      "Node.JS, Express.JS, Nest.JS",
+      "MongoDB, Mongoose, PostgreSQL",
+      "Mongoose, TypeORM",
+    ],
+  },
+  {
+    title: "Other",
+    stack: ["Docker, Webpack, ESLint, Vite", "GitHub, GitLab"],
+  },
+];
--- app/components/technologies/technologies.tsx
+++ app/components/technologies/technologies.tsx
@@ -4,33 +4,23 @@ import "./technologies.scss";
 import Carousel from "./carousel/carousel";
 import ListRevealer from "../animations/listRevealer/listRevealer";
 import SectionRevealer from "../animations/sectionRevealer/sectionRevealer";
+import { stack } from "./stack.data";
 
 export default function Technologies() {
   return (
     <div className="technologies">
       <h2>My Tech Steck</h2>
       <SectionRevealer>
-        <h3>Frontend</h3>
-        <ul>
-          <ListRevealer>
-            <li>React, Next.js</li>
-            <li>HTML, CSS, JavaScript, TypeScript</li>
-            <li>SCSS, Tailwind, MaterialUI</li>
-            <li>Redux, Zustand, Jotai</li>
+        {stack.map((item, i) => (
+          <ListRevealer key={i}>
+            <h3>{item.title}</h3>
+            <ul>
+              {item.stack.map((stackItem, i) => (
+                <li key={i}>{stackItem}</li>
+              ))}
+            </ul>
           </ListRevealer>
-        </ul>
-        <h3>Backend</h3>
-        <ul>
-          <ListRevealer>
-            <li>Node.JS, Express.JS, Nest.JS</li>
-            <li>MongoDB, Mongoose, PostgreSQL</li>
-            <li>Mongoose, TypeORM</li>
-          </ListRevealer>
-        </ul>
-        <h3>Other</h3>
-        <ul>
-          <li>Git, Docker, Webpack, ESLint, Vite, GitLab</li>
-        </ul>
+        ))}
         <Carousel />
       </SectionRevealer>
     </div>
